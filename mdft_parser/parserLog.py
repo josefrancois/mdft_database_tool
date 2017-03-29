b89281b08cf42abe52dbc6cb6ecb719d040c5391
Jose Francois <jose.francois.jf@gmail.com>
2017-03-29 11:03:16 +0200

    Make parserLog.py more readable

--- mdft_parser/parserLog.py
+++ mdft_parser/parserLog.py
@@ -10,17 +10,17 @@ class ParserLog:
 	def parse(self, logfile):
 		with open(logfile, 'r') as flog :
 			for line in flog:
-				if line.find("ENERGY") != -1:
+				if "ENERGY" in line:
 					self.mdft_energy = line.split()[-1]
-				elif line.find("Functional at min") != -1:
+				elif "Functional at min" in line:
 					self.functional_at_min = line.split()[-2]
-				elif line.find("PC ") != -1:
+				elif "PC " in line:
 					self.mdft_energy_pc = line.split()[-2]
-				elif line.find("PC+") != -1:
+				elif "PC+" in line:
 					self.mdft_energy_pc_plus = line.split()[-2]
-				elif line.find("PMV") != -1:
+				elif "PMV" in line:
 					self.mdft_energy_pmv = line.split()[-2]
-				elif line.find("Pid") != -1:
+				elif "Pid" in line:
 					self.mdft_energy_pid = line.split()[-2]
 					
 					
