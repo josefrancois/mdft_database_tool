ca5c59969ab0a53b757d1387115739b487de423e
Jose Francois <jose.francois.jf@gmail.com>
2017-04-07 10:25:05 +0200

    Modify mdft_parse.py which is supposed to be copied in input_mdft directory

--- mdft_parse.py
+++ mdft_parse.py
@@ -4,27 +4,30 @@ import mdft_parser.parserLog as pL
 import mdft_writer.jsonWriter as jW
 
 json_file = "mobley.json"
-folder_to_parse = "./mini_input_mdft/"
+
 pJson = pJ.ParserJson()
 pLog = pL.ParserLog()
 
+solute_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
+
 mdft_database = {}
 
-for subfolder in os.listdir(folder_to_parse):
-	for mdft_file in os.listdir(folder_to_parse+subfolder):
-		if mdft_file[-4:] == ".log":
-			path_to_logfile = folder_to_parse+subfolder+'/'+mdft_file
-			pJson.parseData(json_file, subfolder)
-			pLog.parse(path_to_logfile)
+for solute_dir in solute_dirs:
+	for mdft_file in os.listdir(solute_dir):
+		if mdft_file[-4:] == ".out":
+			log_file = mdft_file
+			pJson.parseData(json_file, solute_dir)
+			pLog.parse(solute_dir+'/'+log_file)
 			data_solute = pJson.getDataSolute()
+			# ALL QUANTITIES IN KCAL/MOL !!!!!!!!
 			data_solute['mdft_energy'] = float(pLog.getMdftEnergy())*0.239006
 			data_solute['functional_at_min'] = float(pLog.getFunctionalAtMin())*0.239006
 			data_solute['mdft_energy_pc'] = float(pLog.getMdftEnergyPc())*0.239006
 			data_solute['mdft_energy_pc+'] = float(pLog.getMdftEnergyPcPlus())*0.239006
 			data_solute['mdft_energy_pmv'] = float(pLog.getMdftEnergyPmv())*0.239006
 			data_solute['mdft_energy_pid'] = float(pLog.getMdftEnergyPid())*0.239006
-			print subfolder
-			mdft_database[subfolder] = data_solute
+			print solute_dir
+			mdft_database[solute_dir] = data_solute
 
 newJson = jW.JsonWriter(mdft_database)
 newJson.write('mdft.json')
