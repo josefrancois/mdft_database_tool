365a11b2585b42d189d24f79eedd15860ed92963
Jose Francois <jose.francois.jf@gmail.com>
2017-04-11 11:49:13 +0200

    Add title to plots

--- mdft_analysis/mdftPlotter.py
+++ mdft_analysis/mdftPlotter.py
@@ -9,7 +9,7 @@ class MdftPlotter:
     def __init__(self, db = None):
         self.database = db
     
-    def plotVS(self, x_column, y_column, x_label, y_label):
+    def plotVS(self, x_column, y_column, x_label, y_label, title):
         self.database.plot.scatter(x_column, y_column, figsize = (5,5))
         limit_min = min(min(self.database[x_column]), min(self.database[y_column])) -10        
         limit_max = max(max(self.database[x_column]), max(self.database[y_column])) +10
@@ -20,6 +20,7 @@ class MdftPlotter:
         fit = np.polyfit(self.database[x_column], self.database[y_column], deg = 1)
         x = self.database[x_column]
         plt.plot(x, fit[0] * x + fit[1], color='green')
+        plt.title(title)
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.legend(["Ideal","Fit equation : y = {1:.3f} + {0:.3f}x\n\t==> R$^2$  = {2:.3f}".format(fit[0], fit[1],self.database[x_column].corr(self.database[y_column]))], shadow = True, edgecolor = 'black')
