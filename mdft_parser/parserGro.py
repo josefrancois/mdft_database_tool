91c751610d900ebf4adb011a2d9cbf4c8fcdaddc
Jose Francois <jose.francois.jf@gmail.com>
2017-04-12 14:59:59 +0200

    Correct initialization of list of coordinates

--- mdft_parser/parserGro.py
+++ mdft_parser/parserGro.py
@@ -1,15 +1,15 @@
 import converter as cv
 
 class ParserGro:  
-    def __init__(self, list_x = [], list_y = [], list_z = []):
-        self.list_x = list_x
-        self.list_y = list_y
-        self.list_z = list_z
+    def __init__(self):
+        self.list_x = []
+        self.list_y = []
+        self.list_z = []
         
     def parseCoord(self, fgro): 
-        converter = cv.Converter()       
         with open(fgro, 'r') as gro:  
             for line in gro:
+                converter = cv.Converter()
                 if line.find("MOL") != -1:
                     self.list_x.append(converter.nmToangstrom(float(line.split()[4])))
                     self.list_y.append(converter.nmToangstrom(float(line.split()[5])))
