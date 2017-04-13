b1143ed3232133693c8a5500468740c88275e9cd
Jose Francois <jose.francois.jf@gmail.com>
2017-04-13 10:37:11 +0200

    Add RMSE in plots' legend and make axis labels clearer

--- mdft_analysis/mdftPlotter.py
+++ mdft_analysis/mdftPlotter.py
@@ -8,7 +8,7 @@ plt.rcParams['figure.figsize'] = (7.0, 7.0)
 class MdftPlotter:
     def __init__(self, db = None):
         self.database = db
-    
+        
     def plotVS(self, x_column, y_column, x_label, y_label, unit, title):
         self.database.plot.scatter(x_column, y_column, figsize = (5,5))
         limit_min = min(min(self.database[x_column]), min(self.database[y_column])) -10        
@@ -23,8 +23,9 @@ class MdftPlotter:
         plt.title(title, fontsize=8)
         plt.xlabel(x_label + " ({0})".format(unit))
         plt.ylabel(y_label + " ({0})".format(unit))
-        plt.legend(["Ideal","Fit equation : y = {1:.3f} + {0:.3f}x\n\t==> R$^2$  = {2:.3f}".format(fit[0], fit[1],self.database[x_column].corr(self.database[y_column]))], shadow = True, edgecolor = 'black')
-        plt.savefig("./mdft_plots/" + y_column + "VS" + x_column  + ".pdf", format="pdf", dpi=1000)
+        rmse = np.sqrt(((self.database[y_column] - self.database[x_column]) ** 2).mean())
+        plt.legend(["Ideal","Fit equation : y = {1:.3f} + {0:.3f}x\n\tR$^2$  = {2:.3f} | RMSE = {3:.3f}".format(fit[0], fit[1], self.database[x_column].corr(self.database[y_column]), rmse)], shadow = True, edgecolor = 'black')
+        plt.savefig("./mdft_plots/" + y_column + "VS" + x_column, format="png", dpi = 130)
 
         
 
