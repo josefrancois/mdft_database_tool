aa12b01dcc276bc5a7977458275af5cf75519448
Jose Francois <jose.francois.jf@gmail.com>
2017-05-02 11:07:29 +0200

    Make parsing of mdft values much more flexible

--- mdft_parse.py
+++ mdft_parse.py
@@ -2,13 +2,11 @@ import os
 import mdft_parser.parserJson as pJ
 import mdft_parser.parserLog as pL
 import mdft_writer.jsonWriter as jW
-import mdft_parser.converter as cv
 
 json_file = "mobley.json"
 
 pJson = pJ.ParserJson()
 pLog = pL.ParserLog()
-converter = cv.Converter()
 
 solute_dirs = [d for d in os.listdir(os.getcwd()) if os.path.isdir(d)]
 
@@ -17,22 +15,11 @@ mdft_database = {}
 for solute_dir in solute_dirs:
 	for mdft_file in os.listdir(solute_dir):
 		if mdft_file[-4:] == ".out":
+            print solute_dir
 			log_file = mdft_file
 			pJson.parseData(json_file, solute_dir)
-			pLog.parse(solute_dir+'/'+log_file)
-			data_solute = pJson.getData()
-            ### All quantities in kcal/mol
-			if pLog.getMdftEnergy() != 'NaN':
-				data_solute['mdft_energy'] = converter.kjTokcal(float(pLog.getMdftEnergy()))
-				data_solute['delta_omega'] = converter.kjTokcal(float(pLog.getFunctionalAtMin()))
-				data_solute['mdft_energy_pc'] = converter.kjTokcal(float(pLog.getMdftEnergyPc()))
-				data_solute['mdft_energy_pc+'] = converter.kjTokcal(float(pLog.getMdftEnergyPcPlus()))
-				#data_solute['mdft_energy_pmv'] = converter.kjTokcal(float(pLog.getMdftEnergyPmv()))
-				#data_solute['mdft_energy_pid'] = converter.kjTokcal(float(pLog.getMdftEnergyPid()))
-				mdft_database[solute_dir] = data_solute
-			else:
-				pass
-			print solute_dir
+			mdft_database[solute_dir] = pLog.parse(solute_dir+'/'+log_file)
+			
 
 newJson = jW.JsonWriter(mdft_database)
 newJson.write('mdft.json')
--- mdft_parser/parserLog.py
+++ mdft_parser/parserLog.py
@@ -1,44 +1,27 @@
+import simplejson as json
+import converter as cv
+
 class ParserLog:
     "Definition of a MDFT output files parser"
     def __init__(self):
-        self.mdft_energy = ''
-        self.functional_at_min = ''
-        self.mdft_energy_pc = ''
-        self.mdft_energy_pc_plus = ''
-        self.mdft_energy_pmv = ''
-        self.mdft_energy_pid = ''
+        with open('references/parameters/mdftParsedValues.json' , 'r') as fjson:
+            self.mdft_values = json.load(fjson)
+            
+            
+        #self.mdft_energy = ''
+        #self.functional_at_min = ''
+        #self.mdft_energy_pc = ''
+        #self.mdft_energy_pc_plus = ''
+        #self.mdft_energy_pmv = ''
+        #self.mdft_energy_pid = ''
         
     def parse(self, logfile):
+        data_log = {}
+        converter = cv.Converter()
         with open(logfile, 'r') as flog :
             for line in flog:
-                if "ENERGY" in line:
-                    self.mdft_energy = line.split()[-1]
-                elif "Functional at min" in line:
-                    self.functional_at_min = line.split()[-2]
-                elif "PC " in line:
-                    self.mdft_energy_pc = line.split()[-2]
-                elif "PC+" in line:
-                    self.mdft_energy_pc_plus = line.split()[-2]
-                elif "PMV" in line:
-                    self.mdft_energy_pmv = line.split()[-2]
-                elif "Pid" in line:
-                    self.mdft_energy_pid = line.split()[-2]
-                    
-                    
-    def getMdftEnergy(self):
-        return self.mdft_energy
-        
-    def getFunctionalAtMin(self):
-        return self.functional_at_min
-        
-    def getMdftEnergyPc(self):
-        return self.mdft_energy_pc
-        
-    def getMdftEnergyPcPlus(self):
-        return self.mdft_energy_pc_plus
-    
-    def getMdftEnergyPmv(self):
-        return self.mdft_energy_pmv
-    
-    def getMdftEnergyPid(self):
-        return self.mdft_energy_pid
+                for mdft_value in self.mdft_values:
+                    if self.mdft_values[mdft_value]['flag'] in line:
+                        ### All quantities in kcal/mol
+                        data_log[mdft_value] = converter.kjTokcal(float(line.split()[self.mdft_values[mdft_value]['position']]))
+        return data_log
