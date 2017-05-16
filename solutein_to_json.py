eda7dfca30975f9b9d3cd2ce412c1a70be05b9c1
Jose Francois <jose.francois.jf@gmail.com>
2017-05-16 10:41:38 +0200

    Add possibility to choose in which database we should add a molecule in solutein_to_json.py

--- solutein_to_json.py
+++ solutein_to_json.py
@@ -5,13 +5,14 @@ import json
 arg_parser = argparse.ArgumentParser(prog="solutein_to_json.py", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
 arg_parser.add_argument("--solutein", help = "solute.in file to parse", default = None)
+arg_parser.add_argument("--database", "-db", help = "Database where the molecule will be added", default = "mdft_database.json")
 mdft_args = arg_parser.parse_args()
 
 solute = SoluteinParser()
 
 molecule = Molecule()
 
-with open('mdft_database.json', 'r') as json_file:
+with open(mdft_args.database, 'r') as json_file:
     mdft_db = json.load(json_file)
     
     if mdft_args.solutein is not None :
@@ -23,9 +24,7 @@ with open('mdft_database.json', 'r') as json_file:
             mdft_db[solute.getName()]['atom'+str((i+1))] = {}
             atom = mdft_db[solute.getName()]['atom'+str((i+1))]
             atom['name'] = 'atom'+str((i+1))
-            #atomtype = solute.getListAtomtype()[i]
             atom['index'] = solute.getListNum()[i]
-            atom['atomtype'] = solute.getListAtomtype()[i]
             atom['x'] = solute.getListx()[i]
             atom['y'] = solute.getListy()[i]
             atom['z'] = solute.getListz()[i]
@@ -33,11 +32,10 @@ with open('mdft_database.json', 'r') as json_file:
             atom['epsilon'] = solute.getListEpsilon()[i]
             atom['sigma'] = solute.getListSigma()[i]
             atom['Zatomic'] = int(solute.getListNumatom()[i])
-            #print atom.getName()
             print mdft_db
             
         newJson = jW.JsonWriter(mdft_db)
-        newJson.write('mdft_database.json')
+        newJson.write(mdft_args.database)
     else:
         pass
 
