a16517b4085e95fcbdbf559940df1a2061a4fcc6
Jose Francois <jose.francois.jf@gmail.com>
2017-05-16 14:14:09 +0200

    Modify mdft_db_analysis.py to use the parameter file database_definition.py to configure the plots

--- mdft_analysis/mdftPlotter.py
+++ mdft_analysis/mdftPlotter.py
@@ -18,22 +18,17 @@ class MdftPlotter:
         self.database.plot.scatter(x_column, y_column, c='black')
         limit_min = min(min(self.database[x_column]), min(self.database[y_column])) -2        
         limit_max = max(max(self.database[x_column]), max(self.database[y_column])) +2
-        #print limit_min, limit_max
         plt.xlim(limit_min, limit_max)
         plt.ylim(limit_min, limit_max)
         plt.plot([limit_min,limit_max], [limit_min,limit_max], lw=0.5, color='black')
         rmse = np.sqrt(((self.database[y_column] - self.database[x_column]) ** 2).mean())
-        #fit = np.polyfit(self.database[x_column], self.database[y_column], deg = 1)
         x = self.database[x_column]
-        #plt.plot(x, fit[0] * x + fit[1], color='green', label="Fit equation : y = {1:.3f} + {0:.3f}x\n\tR$^2$  = {2:.3f} | RMSE = {3:.3f}"\
-        #.format(fit[0], fit[1], self.database[x_column].corr(self.database[y_column]), rmse))
         #plt.title(title+" with {0} solutes".format(self.database.shape[0]), fontsize=8)
         plt.xlabel(x_label + " ({0})".format(unit))
         plt.ylabel(y_label + " ({0})".format(unit))
         #plt.annotate(s='mobley_5857', xy = (self.database.loc['mobley_5857'][x_column], self.database.loc['mobley_5857'][y_column]), xytext=(3, 1.5), arrowprops=dict(facecolor='black', shrink=0.05))
-        plt.text(limit_min+abs(limit_min-limit_max)*0.23, limit_max-abs(limit_min-limit_max)*0.1, "RMSE = {1:.3f}\nPearson R$^2$  = {0:.3f}\nSpearman R$^2$ = {2:.3f}"\
-        .format(self.database[x_column].corr(self.database[y_column]), rmse, self.database[x_column].corr(self.database[y_column], method = 'spearman')), ha='center', va='center', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})
-        #plt.legend(["R$^2$  = {0:.3f} | RMSE = {1:.3f}".format(self.database[x_column].corr(self.database[y_column]), rmse)], shadow = True, edgecolor = 'black', bbox_to_anchor=(1.7, 0.3))
+        plt.text(limit_min+abs(limit_min-limit_max)*0.23, limit_max-abs(limit_min-limit_max)*0.1, "RMSE = {1:.3f}\nPearson R = {0:.3f}\nSpearman R = {2:.3f}"\
+        .format(self.database[x_column].corr(self.database[y_column]), rmse, self.database[x_column].corr(self.database[y_column], method = 'spearman')), ha='center', va='center', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})       
         plt.savefig("./"+self.plots_dir+"/" + y_column + "VS" + x_column +".png", format="png", dpi = 130, bbox_inches='tight', edgecolor='black')
     
     def calcDiffs(self, ref_column, comp_columns_list, ref_label, comp_labels):
@@ -50,38 +45,38 @@ class MdftPlotter:
         sns.violinplot(data=errors_db, orient='h').set_title("Error distribution (kcal/mol)"+" for {0} solutes".format(self.database.shape[0]))
         plt.savefig("./"+self.plots_dir+"/"+name+".png", format="png",bbox_inches='tight')
         
-    def calcErrorGroups(self, x_column, y_columns_list, y_labels):
-        db_groups = pd.DataFrame()
-        error_groups = {}
-        count_groups = {}
-        rel_groups = {}
+    def calcErrorby(self, x_column, y_columns_list, y_labels, cat_column):
+        db_by = pd.DataFrame()
+        error_by = {}
+        count_by = {}
+        rel_by = {}
         labels_list = []
         
         for y_column in y_columns_list:
-            for i, liste in enumerate(self.database['groups']):
-                for group in liste:
-                    count_groups[group] = 0
-                    error_groups[group] = 0
-                    error_groups[group] += abs(self.database[y_column][i]-self.database[x_column][i])
+            for i, liste in enumerate(self.database[cat_column]):
+                for category in liste:
+                    count_by[category] = 0
+                    error_by[category] = 0
+                    error_by[category] += abs(self.database[y_column][i]-self.database[x_column][i])
                     
-            for liste in self.database['groups']:
-                for group in liste:
-                    count_groups[group] += 1
+            for liste in self.database[cat_column]:
+                for category in liste:
+                    count_by[category] += 1
 
-            for group in error_groups:
-                rel_groups["({0}) ".format(count_groups[group])+group] = error_groups[group] / count_groups[group]
+            for category in error_by:
+                rel_by["({0}) ".format(count_by[category])+category] = error_by[category] / count_by[category]
 
-            db_groups = pd.concat([db_groups, pd.DataFrame.from_dict(rel_groups, orient='index')], axis=1)
+            db_by = pd.concat([db_by, pd.DataFrame.from_dict(rel_by, orient='index')], axis=1)
             labels_list.append(y_labels[y_column])
             
-        db_groups.columns = labels_list
-        return db_groups
+        db_by.columns = labels_list
+        return db_by
         
-    def plotErrorGroups(self, db_groups):
-        db_groups.plot.bar(figsize=(30,15), yticks=(range(0,10,2)))
+    def plotErrorby(self, db_by, cat_column):
+        db_by.plot.bar(figsize=(30,15), yticks=(range(0,10,2)))
         plt.ylabel('AUE (kcal/mol)')
-        plt.title('Average Unsigned Error (AUE) due to functional groups with respect to Experiment relative to number of molecules')
-        plt.savefig("./"+self.plots_dir+"/error_groups.png", format="png", bbox_inches='tight')
+        plt.title('Average Unsigned Error (AUE) due to functional by with respect to Experiment relative to number of molecules')
+        plt.savefig("./"+self.plots_dir+"/error_by_"+cat_column+".png", format="png", bbox_inches='tight')
 """        
     def plotEnrichmentCurve(self, x_column, y_column):
         diff = abs(self.database[x_column] - self.database[y_column])
--- mdft_db_analysis.py
+++ mdft_db_analysis.py
@@ -5,50 +5,51 @@ import json
 import argparse
 
 arg_parser = argparse.ArgumentParser(prog="mdft_db_analysis.py", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
-arg_parser.add_argument("--json", help = "JSON file to parse", default = "mdft.json")
+#arg_parser.add_argument("--json", help = "JSON file to parse", default = "mdft.json")
+arg_parser.add_argument("--mdft_database", "-mdft_db", help = "Database for which MDFT will be compared", default = "mobley")
 mdft_args = arg_parser.parse_args()
 
-mdft_db = pd.read_json(mdft_args.json, orient='index')
+
+with open("database_definition.json", 'r') as json_file:
+    db_def = json.load(json_file)
+    db_values = db_def[mdft_args.mdft_database]['values_to_parse']
+    mdft_json = db_def[mdft_args.mdft_database]["mdft_output"]
+    mdft_db = pd.read_json(mdft_json, orient='index')
+    plots = db_def[mdft_args.mdft_database]['plots']
+    plots_vs = plots['plotvs']
+    plots_errdistrib = plots['errordistrib']
+    plots_errby = plots['errorby']
+    unit = plots['unit']
+    
+#mdft_db = pd.read_json(mdft_args.json, orient='index')
 
 print mdft_db.shape[0], "solutes"
 
-plots_vs = {}
-plots_errdistrib = {}
-plots_errgroups = {}
-unit = ''
-with open("references/parameters/mdftPlotConfig.json", 'r') as json_plots:
-    plots = json.load(json_plots)
-    plots_vs = plots['plotvs']
-    plots_errdistrib = plots['errordistrib']
-    plots_errgroups = plots['errorgroups']
-    unit = plots['unit']
 
-
-values_label = {}    
+values_label = {}
+for value in db_values:
+    values_label[value] = db_values[value]['label']    
 with open("references/parameters/mdftParsedValues.json", 'r') as json_values:
-    values = json.load(json_values)
-    for value in values:
-        values_label[value] = values[value]['label']
+    mdft_values = json.load(json_values)
+    for value in mdft_values:
+        values_label[value] = mdft_values[value]['label']
 
 
-plots_dir = mdft_args.json[:-5]+"_plots"
+plots_dir = mdft_json[:-5]+"_plots"
 os.mkdir(plots_dir)
 plotter = mP.MdftPlotter(mdft_db, plots_dir)
 
 
 for plot in plots_vs:
     for value in plots_vs[plot]['y'].split():
-        if plots_vs[plot]['x_label'] not in values_label:
-            values_label[plots_vs[plot]['x']] = plots_vs[plot]['x_label']
-        plotter.plotVS(plots_vs[plot]['x'], value, plots_vs[plot]['x_label'], values_label[value], unit)
+        plotter.plotVS(plots_vs[plot]['x'], value, values_label[plots_vs[plot]['x']], values_label[value], unit)
         
 for plot in plots_errdistrib:
-    values_list = plots_errdistrib[plot]['comp'].split()
-    plotter.plotErrorDistribution(plotter.calcDiffs(plots_errdistrib[plot]['ref'], values_list, plots_errdistrib[plot]["ref_label"], values_label), plots_errdistrib[plot]['name'])
+    values_list = plots_errdistrib[plot]['y'].split()
+    plotter.plotErrorDistribution(plotter.calcDiffs(plots_errdistrib[plot]['x'], values_list, values_label[plots_errdistrib[plot]['x']], values_label), plots_errdistrib[plot]['name'])
 
-for plot in plots_errgroups:
-    values_list = plots_errgroups[plot]['y'].split()
-    plotter.plotErrorGroups(plotter.calcErrorGroups(plots_errgroups[plot]['x'], values_list, values_label)) 
+for plot in plots_errby:
+    values_list = plots_errby[plot]['y'].split()
+    plotter.plotErrorby(plotter.calcErrorby(plots_errby[plot]['x'], values_list, values_label, plots_errby[plot]['cat_column']), plots_errby[plot]['cat_column']) 
     
-os.system("cp " + mdft_args.json + " ./"+plots_dir)
-
+os.system("cp " + mdft_json + " ./"+plots_dir)
