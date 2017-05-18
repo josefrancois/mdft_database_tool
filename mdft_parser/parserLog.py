8a1748a5fd4178baa152045b82e4c6a1d106233a
Jose Francois <jose.francois.jf@gmail.com>
2017-05-18 15:13:51 +0200

    Add an encoding command to ignore non-ASCII character from mdftParsedValues.json

--- mdft_parser/parserLog.py
+++ mdft_parser/parserLog.py
@@ -1,19 +1,22 @@
-import simplejson as json
+# -*- coding: utf-8 -*- 
+
+import json
 import converter as cv
 
 class ParserLog:
     "Definition of a MDFT output files parser"
     def __init__(self):
         with open('references/parameters/mdftParsedValues.json' , 'r') as fjson:
-            self.mdft_values = json.load(fjson)
+            self.mdft_values = json.load(fjson) 
         
     def parse(self, logfile):
         data_log = {}
         converter = cv.Converter()
-        with open(logfile, 'r') as flog :
+	    with open(logfile, 'r') as flog :
             for line in flog:
-                for mdft_value in self.mdft_values:
-                    if self.mdft_values[mdft_value]['flag'] in line:
-                        ### All quantities in kcal/mol
+                for mdft_value in self.mdft_values:	
+                    if self.mdft_values[mdft_value]['flag'].encode('ascii', 'ignore') in line: #To avoid any decoding error from 'mdftParsedValues.json' file reading	
+		                ### All quantities in kcal/mol
                         data_log[mdft_value] = converter.kjTokcal(float(line.split()[self.mdft_values[mdft_value]['position']]))
-        return data_log
+        	
+	return data_log
