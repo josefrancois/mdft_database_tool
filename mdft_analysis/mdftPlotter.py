0f772bcd466df5e4d01985d6cd18377469e9356a
Jose Francois <jose.francois.jf@gmail.com>
2017-06-15 22:12:21 +0200

    Modify mdftPlotter.py : enhance axis labels with LaTeX, add the plot of pbias related to a categorical variable, add vertical line on error distribution plots and add some statistical values on the legend of correlation plots

--- mdft_analysis/mdftPlotter.py
+++ mdft_analysis/mdftPlotter.py
@@ -7,7 +7,7 @@ HEIGHT_FIGSIZE = 7.0
 WIDTH_FIGSIZE = 7.0
 plt.style.use('classic')
 plt.rcParams['figure.figsize'] = (7.0, 7.0)
-
+plt.rcParams['axes.labelsize'] = 20
 
 class MdftPlotter:
     def __init__(self, db = None, plots_dir = None):
@@ -22,27 +22,41 @@ class MdftPlotter:
         plt.ylim(limit_min, limit_max)
         plt.plot([limit_min,limit_max], [limit_min,limit_max], lw=0.5, color='black')
         rmse = np.sqrt(((self.database[y_column] - self.database[x_column]) ** 2).mean())
+        rsr = rmse/self.database[x_column].std()
+        pearson_r = self.database[x_column].corr(self.database[y_column])
+        spearman_rho = self.database[x_column].corr(self.database[y_column], method = 'spearman')
+        kendall_tau = self.database[x_column].corr(self.database[y_column], method = 'kendall')
+        r2 = pearson_r**2
         x = self.database[x_column]
         #plt.title(title+" with {0} solutes".format(self.database.shape[0]), fontsize=8)
-        plt.xlabel(x_label + " ({0})".format(unit))
-        plt.ylabel(y_label + " ({0})".format(unit))
+        plt.xlabel("$\mathrm{\mathsf{\Delta G^{"+x_label+"}_{solv}}}$" + " ({0})".format(unit), fontsize=16)
+        plt.ylabel("$\mathrm{\mathsf{\Delta G^{"+y_label+"}_{solv}}}$" + " ({0})".format(unit), fontsize=16)
         #plt.annotate(s='mobley_5857', xy = (self.database.loc['mobley_5857'][x_column], self.database.loc['mobley_5857'][y_column]), xytext=(3, 1.5), arrowprops=dict(facecolor='black', shrink=0.05))
-        plt.text(limit_min+abs(limit_min-limit_max)*0.23, limit_max-abs(limit_min-limit_max)*0.1, "RMSE = {1:.3f}\nPearson R = {0:.3f}\nSpearman R = {2:.3f}"\
-        .format(self.database[x_column].corr(self.database[y_column]), rmse, self.database[x_column].corr(self.database[y_column], method = 'spearman')), ha='center', va='center', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})       
-        plt.savefig("./"+self.plots_dir+"/" + y_column + "VS" + x_column +".png", format="png", dpi = 130, bbox_inches='tight', edgecolor='black')
+        
+        pbias = 0
+        for i in range(self.database.shape[0]):
+            pbias += (self.database[x_column][i] - self.database[y_column][i])*100
+        pbias = pbias / self.database[x_column].sum()
+            
+        plt.text(limit_min+abs(limit_min-limit_max)*0.23, limit_max-abs(limit_min-limit_max)*0.15, \
+        "RMSE = {0:.2f} {9:s}\n P Bias = {1:.2f} %\nPearson R = {2:.2f}\nSpearman {3:s} = {4:.2f}\nKendall {5:s} = {6:.2f}\nR{7:s} = {8:.2f}"\
+        .format(rmse, pbias, pearson_r, r'$\rho$', spearman_rho, r'$\tau$', kendall_tau, r'$^2$', r2, unit), ha='center', va='center', bbox={'facecolor':'white', 'alpha':0.5, 'pad':10})       
+        plt.savefig(self.plots_dir+"/" + y_column + "VS" + x_column +".png", format="png", dpi = 200, bbox_inches='tight', edgecolor='black')
     
     def calcDiffs(self, ref_column, comp_columns_list, ref_label, comp_labels):
         diff_db = pd.DataFrame()
         diff_db_labels = []
         for comp_column in comp_columns_list:
             diff_db = pd.concat([diff_db, self.database[comp_column]-self.database[ref_column]], axis=1)
-            diff_db_labels.append(comp_labels[comp_column] + ' - ' + ref_label)
+            diff_db_labels.append("$\mathrm{\Delta G^{"+comp_labels[comp_column]+"}_{solv}}$" + ' - ' + "$\mathrm{\Delta G^{"+ref_label+"}_{solv}}$")
         diff_db.columns = diff_db_labels
         return diff_db
         
     def plotErrorDistribution(self, errors_db, name):   
-        plt.figure()          
-        sns.violinplot(data=errors_db, orient='h').set_title("Error distribution (kcal/mol)"+" for {0} solutes".format(self.database.shape[0]))
+        plt.figure()         
+        sns.violinplot(data=errors_db, orient='h',zorder=1, split=True)
+        plt.xlim(-20, 20)
+        plt.axvline(0, color = 'black', linestyle='dotted', linewidth=0.7)
         plt.savefig("./"+self.plots_dir+"/"+name+".png", format="png",bbox_inches='tight')
         
     def calcErrorby(self, x_column, y_columns_list, y_labels, cat_column):
@@ -64,19 +78,61 @@ class MdftPlotter:
                     count_by[category] += 1
 
             for category in error_by:
-                rel_by["({0}) ".format(count_by[category])+category] = error_by[category] / count_by[category]
+                if category != '':
+                    rel_by["({0}) ".format(count_by[category])+category] = error_by[category] / count_by[category]
 
             db_by = pd.concat([db_by, pd.DataFrame.from_dict(rel_by, orient='index')], axis=1)
             labels_list.append(y_labels[y_column])
             
         db_by.columns = labels_list
+        db_by = db_by.sort_values(by=labels_list[-1], ascending=True)
         return db_by
         
+        
     def plotErrorby(self, db_by, cat_column):
-        db_by.plot.bar(figsize=(30,15), yticks=(range(0,10,2)))
+        db_by[db_by.index != ''].plot.barh(figsize=(15,25), xticks=(range(0,8)))
         plt.ylabel('AUE (kcal/mol)')
-        plt.title('Average Unsigned Error (AUE) due to functional by with respect to Experiment relative to number of molecules')
+        plt.legend(loc='lower right')
+        #plt.title('Average Unsigned Error (AUE) due to functional by with respect to Experiment relative to number of molecules')
         plt.savefig("./"+self.plots_dir+"/error_by_"+cat_column+".png", format="png", bbox_inches='tight')
+        
+    def calcPbiasby(self, x_column, y_columns_list, y_labels, cat_column):
+        db_pbias_by = pd.DataFrame()
+        error100_by = {}
+        sum_x_column_by = {}
+        count_by = {}
+        pbias_by = {}
+        labels_list = []
+        
+        for y_column in y_columns_list:
+            for i, liste in enumerate(self.database[cat_column]):
+                for category in liste:
+                    sum_x_column_by[category] = 0
+                    count_by[category] = 0
+                    error100_by[category] = 0
+                    error100_by[category] += (self.database[x_column][i]-self.database[y_column][i])*100
+                    sum_x_column_by[category] += self.database[x_column][i]
+                    
+            for liste in self.database[cat_column]:
+                for category in liste:
+                    count_by[category] += 1
+
+            for category in error100_by:
+                pbias_by["({0}) ".format(count_by[category])+category] = error100_by[category] / sum_x_column_by[category]
+                #print category, error100_by[category], sum_x_column_by[category]    
+            db_pbias_by = pd.concat([db_pbias_by, pd.DataFrame.from_dict(pbias_by, orient='index')], axis=1)
+            labels_list.append(y_labels[y_column])
+            
+        db_pbias_by.columns = labels_list
+        return db_pbias_by
+        
+    def plotPbiasby(self, db_pbias_by, cat_column):
+        db_pbias_by.plot.bar(figsize=(30,15)) #yticks=(range(0,10,2))
+        plt.ylabel('Pbias (%)')
+        plt.title('Pbias due to functional by with respect to Experiment relative to number of molecules')
+        plt.savefig("./"+self.plots_dir+"/pbias_by_"+cat_column+".png", format="png", bbox_inches='tight')
+        
+     
 """        
     def plotEnrichmentCurve(self, x_column, y_column):
         diff = abs(self.database[x_column] - self.database[y_column])
