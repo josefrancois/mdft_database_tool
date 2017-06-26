722c873b9daec2b1e4acb1236a89e3d2f4f39716
Jose Francois <jose.francois.jf@gmail.com>
2017-06-26 10:47:55 +0200

    Remove converter.py from mdft_analysis

--- mdft_analysis/converter.py
+++
@@ -1,9 +0,0 @@
-class Converter:
-    def kjTokcal(self, energy_kj):
-        return energy_kj * 0.239006
-        
-    def nmToangstrom(self, length):
-        return length * 10
-        
-    def kcalTokj(self, energy_kcal):
-        return energy_kcal * 4.187
