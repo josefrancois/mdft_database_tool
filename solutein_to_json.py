9420261e3fcbb0b48b247e16c7adce678ef1faa3
Jose Francois <jose.francois.jf@gmail.com>
2017-06-29 11:47:53 +0200

    Add commentaries showing the command to use solutein_to_json.py on several solute.in files stored in a folder

--- solutein_to_json.py
+++ solutein_to_json.py
@@ -2,8 +2,9 @@ import argparse
 from mdft_parser.parserSolutein import *
 import mdft_writer.jsonWriter as jW
 import json
-arg_parser = argparse.ArgumentParser(prog="solutein_to_json.py", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+# Set solutein and databases options to know which solute.in file to parse and to which JSON database the data should be added
+arg_parser = argparse.ArgumentParser(prog="solutein_to_json.py", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 arg_parser.add_argument("--solutein", help = "solute.in file to parse", default = None)
 arg_parser.add_argument("--database", "-db", help = "Database where the molecule will be added", default = "mdft_database.json")
 mdft_args = arg_parser.parse_args()
@@ -21,7 +22,7 @@ with open(mdft_args.database, 'r') as json_file:
         mdft_db[solute.getName()] = {}
         print solute.getName()
         for i in range(solute.getNumberOfAtoms()):         
-            mdft_db[solute.getName()]['atom'+str((i+1))] = {}
+            mdft_db[solute.getName()]['atom'+str((i+1))] = {} # Creating one dictionary for each atom of the solute
             atom = mdft_db[solute.getName()]['atom'+str((i+1))]
             atom['name'] = 'atom'+str((i+1))
             atom['index'] = solute.getListNum()[i]
@@ -39,4 +40,5 @@ with open(mdft_args.database, 'r') as json_file:
     else:
         pass
 
-
+# Bash script to execute it on several solute.in files stored in a folder:
+# for folder in *; do if [ -d $folder -a $folder == "**Name of the folder**" ]; then for molecule in $folder/*; do python solutein_to_json.py -db  **Name of the JSON database**.json --solutein $molecule; done fi done
